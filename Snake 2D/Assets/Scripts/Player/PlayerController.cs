b707b28b6c407e8d5acb8a24e551152f0c0fa1ff
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-24 11:56:21 +0530

    testing stuff

--- Snake 2D/Assets/Scripts/Player/PlayerController.cs
+++ Snake 2D/Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidBody;
     [SerializeField] private Transform _snakeFace;
+    [SerializeField] private Transform _snakeBody;
 
     private float _speed = 2.0f;
     private MovementDirection _movementDirection;
@@ -21,25 +22,21 @@ public class PlayerController : MonoBehaviour
         {
             _rigidBody.velocity = Vector2.up * _speed;
             _movementDirection = MovementDirection.UP;
-            _snakeFace.Rotate(0, 0, 90);
         }
         else if (Input.GetKeyDown(KeyCode.D) && _movementDirection != MovementDirection.LEFT)
         {
             _rigidBody.velocity = Vector2.right * _speed;
             _movementDirection = MovementDirection.RIGHT;
-            _snakeFace.Rotate(0, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.A) && _movementDirection != MovementDirection.RIGHT)
         {
             _rigidBody.velocity = Vector2.left * _speed;
             _movementDirection = MovementDirection.LEFT;
-            _snakeFace.Rotate(0, 0, 180);
         }
         else if (Input.GetKeyDown(KeyCode.S) && _movementDirection != MovementDirection.UP)
         {
             _rigidBody.velocity = Vector2.down * _speed;
             _movementDirection = MovementDirection.DOWN;
-            _snakeFace.Rotate(0, 0, -90);
         }
     }
 }
