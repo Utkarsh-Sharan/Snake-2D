c1dd94464335fc3e15e3c5ac10337f5bbea8fbb7
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-26 11:18:52 +0530

    implemented snake body wrap

--- Snake 2D/Assets/Scripts/Player/PlayerController.cs
+++ Snake 2D/Assets/Scripts/Player/PlayerController.cs
@@ -5,35 +5,64 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform _snakeBody;
+    [SerializeField] private Camera _camera;
 
     private int _snakeBodySize;
 
     private Vector2 _snakeMove;
     private MovementDirection _movementDirection;
 
+    private float _screenWidth;
+    private float _screenHeight;
+
     private List<Transform> _snakeBodyTransformList;
 
     private void Start()
     {
-        transform.position = new Vector3(10f, 10f, 0f);
+        transform.position = new Vector3(0f, 0f, 0f);
 
         _snakeBodyTransformList = new List<Transform>();
         _snakeBodyTransformList.Add(this.transform);
+
+        float cameraHeight = _camera.orthographicSize * 2;  //as camera's orthographic size is half the height
+        float cameraWidth = cameraHeight * _camera.aspect;  //as camera's aspect ratio = width/height
+
+        _screenWidth = cameraWidth / 2;
+        _screenHeight = cameraHeight / 2;
     }
 
     private void Update()
     {
         Movement();
-        if(Input.GetMouseButtonDown(0))
+        WrapSnake();
+        if (Input.GetMouseButtonDown(0))    //only for testing
         {
             _snakeBodySize++;
             CreateSnakeBody();
         }
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(1))     //only for testing
         {
             _snakeBodySize--;
             DestroySnakeBody();
         }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //adds body parts from back
+        for (int i = _snakeBodyTransformList.Count - 1; i > 0; i--)
+        {
+            _snakeBodyTransformList[i].position = _snakeBodyTransformList[i - 1].position;
+        }
+        
+        //rigidbody movement
+        this.transform.position = new Vector3(
+                                         Mathf.Round(this.transform.position.x + _snakeMove.x),
+                                         Mathf.Round(this.transform.position.y + _snakeMove.y),
+                                         0f);
+        //wrapping snake's body
+        WrapSnake();
     }
 
     private void Movement()
@@ -63,21 +92,6 @@ public class PlayerController : MonoBehaviour
         HandleSnakeFaceDirection(_movementDirection);
     }
 
-    private void FixedUpdate()
-    {
-        //adds body parts from back
-        for (int i = _snakeBodyTransformList.Count - 1; i > 0; i--)
-        {
-            _snakeBodyTransformList[i].position = _snakeBodyTransformList[i - 1].position;
-        }
-
-        //rigidbody movement
-        this.transform.position = new Vector3(
-                                         Mathf.Round(this.transform.position.x + _snakeMove.x),
-                                         Mathf.Round(this.transform.position.y + _snakeMove.y),
-                                         0f);
-    }
-
     private void HandleSnakeFaceDirection(MovementDirection faceDirection)
     {
         switch (faceDirection)
@@ -101,6 +115,31 @@ public class PlayerController : MonoBehaviour
                 transform.eulerAngles = new Vector3(0, 0, -90);
                 break;
         }
+    }
+
+    private void WrapSnake()
+    {
+        Vector3 headPosition = _snakeBodyTransformList[0].position;
+
+        if (headPosition.x > _screenWidth)
+        {
+            headPosition.x = -_screenWidth;
+        }
+        else if (headPosition.x < -_screenWidth)
+        {
+            headPosition.x = _screenWidth;
+        }
+
+        if (headPosition.y > _screenHeight)
+        {
+            headPosition.y = -_screenHeight;
+        }
+        else if (headPosition.y < -_screenHeight)
+        {
+            headPosition.y = _screenHeight;
+        }
+
+        _snakeBodyTransformList[0].position = headPosition;
     }
 
     public void CreateSnakeBody()
