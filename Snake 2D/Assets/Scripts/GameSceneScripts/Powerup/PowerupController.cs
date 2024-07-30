52f7a3f2332d6850a10e8e5a075d6634bcada23f
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-30 11:34:38 +0530

    animated powerups through Update()

--- Snake 2D/Assets/Scripts/GameSceneScripts/Powerup/PowerupController.cs
+++ Snake 2D/Assets/Scripts/GameSceneScripts/Powerup/PowerupController.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PowerupController : MonoBehaviour
 {
     [SerializeField] private GameObject[] _powerupObjects = new GameObject[3];
+
+    private float minimumYPos = 0.1f;
+    private float maximumYPos = 0.3f;
+    private float yPosition;
+    private float bounceSpeed = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +33,13 @@ public class PowerupController : MonoBehaviour
     // Update is called once per frame
     protected void Update()
     {
-        
+        // Calculate the sine value based on time and bounceSpeed
+        float sinValue = Mathf.Sin(Time.time * bounceSpeed);
+
+        // Interpolate between maximum and minimum Y positions
+        yPosition = Mathf.Lerp(maximumYPos, minimumYPos, Mathf.Abs(sinValue));
+
+        // Update the power-up's position
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
     }
 }
