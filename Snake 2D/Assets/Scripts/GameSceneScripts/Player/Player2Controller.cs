f7437af652f3fcfc2d9deab707082d22cb1f5ef1
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-08-02 10:56:22 +0530

    both players moving as intended

--- Snake 2D/Assets/Scripts/GameSceneScripts/Player/Player2Controller.cs
+++ Snake 2D/Assets/Scripts/GameSceneScripts/Player/Player2Controller.cs
@@ -2,18 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Controller : PlayerController
+public class Player2Controller : PlayerController
 {
     protected override void Start()
     {
-        if(GameManager.Instance.GameType == GameType.SINGLE_PLAYER)
-        {
-            transform.position = new Vector3(0f, 0f, 0f);       //snake's initial position
-        }
-        else
-        {
-            transform.position = new Vector3(-5f, 0f, 0f);
-        }
+        transform.position = new Vector3(5f, 0f, 0f);       //snake's initial position
 
         snakeBodyTransformList = new List<Transform>();    //initializing transform list
         snakeBodyTransformList.Add(this.transform);        //adding snake's head to the list
@@ -28,22 +21,22 @@ public class Player1Controller : PlayerController
     protected override void Movement()
     {
         //handling inputs in update
-        if (Input.GetKeyDown(KeyCode.W) && movementDirection != MovementDirection.DOWN)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && movementDirection != MovementDirection.DOWN)
         {
             snakeMove = Vector2.up;
             movementDirection = MovementDirection.UP;
         }
-        else if (Input.GetKeyDown(KeyCode.D) && movementDirection != MovementDirection.LEFT)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && movementDirection != MovementDirection.LEFT)
         {
             snakeMove = Vector2.right;
             movementDirection = MovementDirection.RIGHT;
         }
-        else if (Input.GetKeyDown(KeyCode.A) && movementDirection != MovementDirection.RIGHT)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && movementDirection != MovementDirection.RIGHT)
         {
             snakeMove = Vector2.left;
             movementDirection = MovementDirection.LEFT;
         }
-        else if (Input.GetKeyDown(KeyCode.S) && movementDirection != MovementDirection.UP)
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && movementDirection != MovementDirection.UP)
         {
             snakeMove = Vector2.down;
             movementDirection = MovementDirection.DOWN;
--- Snake 2D/Assets/Scripts/GameSceneScripts/Player/PlayerController.cs
+++ Snake 2D/Assets/Scripts/GameSceneScripts/Player/PlayerController.cs
@@ -2,35 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public abstract class PlayerController : MonoBehaviour
 {
-    [SerializeField] protected Transform _snakeBody;
-    [SerializeField] protected Camera _camera;
-    [SerializeField] protected GameObject _gameOverUIPanel;
+    [SerializeField] protected Transform snakeBody;
+    [SerializeField] protected Camera mainCamera;
+    [SerializeField] protected GameObject gameOverUIPanel;
 
-    protected int _snakeBodySize;
-    protected Vector2 _snakeMove;
-    protected MovementDirection _movementDirection;
+    protected int snakeBodySize;
+    protected Vector2 snakeMove;
+    protected MovementDirection movementDirection;
 
-    protected float _screenWidth;
-    protected float _screenHeight;
-    protected float _boundaryCheckOffset = 1f;
+    protected float screenWidth;
+    protected float screenHeight;
+    protected float boundaryCheckOffset = 1f;
 
-    protected List<Transform> _snakeBodyTransformList;
+    protected List<Transform> snakeBodyTransformList;
 
-    protected void Start()
-    {
-        transform.position = new Vector3(0f, 0f, 0f);       //snake's initial position
-
-        _snakeBodyTransformList = new List<Transform>();    //initializing transform list
-        _snakeBodyTransformList.Add(this.transform);        //adding snake's head to the list
-
-        float cameraHeight = _camera.orthographicSize * 2;  //as camera's orthographic size is half the height
-        float cameraWidth = cameraHeight * _camera.aspect;  //as camera's aspect ratio = width/height
-
-        _screenWidth = cameraWidth / 2;
-        _screenHeight = cameraHeight / 2;
-    }
+    protected abstract void Start();
 
     protected void Update()
     {
@@ -41,47 +29,22 @@ public class PlayerController : MonoBehaviour
     protected void FixedUpdate()
     {
         //adds body parts from back
-        for (int i = _snakeBodyTransformList.Count - 1; i > 0; i--)
+        for (int i = snakeBodyTransformList.Count - 1; i > 0; i--)
         {
-            _snakeBodyTransformList[i].position = _snakeBodyTransformList[i - 1].position;
+            snakeBodyTransformList[i].position = snakeBodyTransformList[i - 1].position;
         }
 
         //rigidbody movement
         this.transform.position = new Vector3(
-                                         Mathf.Round(this.transform.position.x + _snakeMove.x),
-                                         Mathf.Round(this.transform.position.y + _snakeMove.y),
+                                         Mathf.Round(this.transform.position.x + snakeMove.x),
+                                         Mathf.Round(this.transform.position.y + snakeMove.y),
                                          0f);
 
         //wrapping snake's body
         WrapSnakeHead();
     }
 
-    protected void Movement()
-    {
-        //handling inputs in update
-        if (Input.GetKeyDown(KeyCode.W) && _movementDirection != MovementDirection.DOWN)
-        {
-            _snakeMove = Vector2.up;
-            _movementDirection = MovementDirection.UP;
-        }
-        else if (Input.GetKeyDown(KeyCode.D) && _movementDirection != MovementDirection.LEFT)
-        {
-            _snakeMove = Vector2.right;
-            _movementDirection = MovementDirection.RIGHT;
-        }
-        else if (Input.GetKeyDown(KeyCode.A) && _movementDirection != MovementDirection.RIGHT)
-        {
-            _snakeMove = Vector2.left;
-            _movementDirection = MovementDirection.LEFT;
-        }
-        else if (Input.GetKeyDown(KeyCode.S) && _movementDirection != MovementDirection.UP)
-        {
-            _snakeMove = Vector2.down;
-            _movementDirection = MovementDirection.DOWN;
-        }
-
-        HandleSnakeFaceDirection(_movementDirection);
-    }
+    protected abstract void Movement();
 
     protected void HandleSnakeFaceDirection(MovementDirection faceDirection)
     {
@@ -110,29 +73,29 @@ public class PlayerController : MonoBehaviour
 
     protected void WrapSnakeHead()
     {
-        Vector3 snakeHeadPosition = _snakeBodyTransformList[0].position;
+        Vector3 snakeHeadPosition = snakeBodyTransformList[0].position;
 
         //checking at x-axis
-        if (snakeHeadPosition.x > _screenWidth)
+        if (snakeHeadPosition.x > screenWidth)
         {
-            snakeHeadPosition.x = -_screenWidth;
+            snakeHeadPosition.x = -screenWidth;
         }
-        else if (snakeHeadPosition.x < -_screenWidth)
+        else if (snakeHeadPosition.x < -screenWidth)
         {
-            snakeHeadPosition.x = _screenWidth;
+            snakeHeadPosition.x = screenWidth;
         }
 
         //checking at y-axis
-        if (snakeHeadPosition.y > _screenHeight)
+        if (snakeHeadPosition.y > screenHeight)
         {
-            snakeHeadPosition.y = -_screenHeight;
+            snakeHeadPosition.y = -screenHeight;
         }
-        else if (snakeHeadPosition.y < -_screenHeight)
+        else if (snakeHeadPosition.y < -screenHeight)
         {
-            snakeHeadPosition.y = _screenHeight;
+            snakeHeadPosition.y = screenHeight;
         }
 
-        _snakeBodyTransformList[0].position = snakeHeadPosition;
+        snakeBodyTransformList[0].position = snakeHeadPosition;
     }
 
     protected void OnCollisionEnter2D(Collision2D other)
@@ -144,42 +107,42 @@ public class PlayerController : MonoBehaviour
         else if (other.gameObject.GetComponent<SnakeBodyPart>())
         {
             SoundManager.Instance.PlayMusic(Sounds.PLAYER_DEATH);
-            GameManager.Instance.GameOverHandler(_gameOverUIPanel);
+            GameManager.Instance.GameOverHandler(gameOverUIPanel);
         }
     }
 
     protected bool IsWithinBoundaryExclusionZone(Vector3 position)
     {
         // Check if the snake head is within the exclusion zone of the leftmost or rightmost boundary
-        return position.x <= -_screenWidth + _boundaryCheckOffset ||
-               position.x >= _screenWidth - _boundaryCheckOffset;
+        return position.x <= -screenWidth + boundaryCheckOffset ||
+               position.x >= screenWidth - boundaryCheckOffset;
     }
 
     public void CreateSnakeBody()
     {
-        _snakeBodySize++;
+        snakeBodySize++;
 
         //instantiating and storing the body
-        Transform snakeBody = Instantiate(this._snakeBody);
+        Transform snakeBody = Instantiate(this.snakeBody);
 
         //setting the body position
-        snakeBody.position = _snakeBodyTransformList[_snakeBodyTransformList.Count - 1].position;
+        snakeBody.position = snakeBodyTransformList[snakeBodyTransformList.Count - 1].position;
 
         //adding it to the list
-        _snakeBodyTransformList.Add(snakeBody);
+        snakeBodyTransformList.Add(snakeBody);
     }
 
     public void DestroySnakeBody()
     {
-        if (_snakeBodyTransformList.Count > 1) // There must be at least one body part to remove
+        if (snakeBodyTransformList.Count > 1) // There must be at least one body part to remove
         {
-            _snakeBodySize--;
+            snakeBodySize--;
 
             // Getting the last body part
-            Transform lastBodyPart = _snakeBodyTransformList[_snakeBodyTransformList.Count - 1];
+            Transform lastBodyPart = snakeBodyTransformList[snakeBodyTransformList.Count - 1];
 
             // Removing it from the list
-            _snakeBodyTransformList.RemoveAt(_snakeBodyTransformList.Count - 1);
+            snakeBodyTransformList.RemoveAt(snakeBodyTransformList.Count - 1);
 
             // Destroying that body part
             Destroy(lastBodyPart.gameObject);
